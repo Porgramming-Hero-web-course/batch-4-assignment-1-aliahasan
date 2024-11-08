fe4c7e162dff7497aefcf8f2e5a612abd66e2d79
aliahasan <aanabin18@gmail.com>
2024-11-08 14:50:31 +0600

    minor updated

--- problems/problem-8.ts
+++ problems/problem-8.ts
@@ -1,16 +1,17 @@
-interface Person {
+interface Person1 {
   name: string;
   age: number;
   email: string;
+  isAdmin?: boolean;
 }
 
 function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
   return keys.every((key) => key in obj);
 }
-const person: Person = {
+const person1: Person1 = {
   name: "Alice",
   age: 25,
   email: "alice@example.com",
 };
 
-const results = validateKeys(person, ["name", "age"]);
+const results = validateKeys(person1, ["name", "age"]);
